d418657e5f7c721e3d1eea2075c1fe8b2bcd2b0a
Nils Harbke <nils.harbke@proton.me>
2024-04-04 01:00:13 +0200

    update

--- flake.nix
+++ flake.nix
@@ -1,138 +1,72 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
 
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.rust-analyzer-src.follows = "";
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
-
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs = {
-      nixpkgs.follows = "nixpkgs";
-      flake-utils.follows = "flake-utils";
-    };
-
-};
-
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, fenix, ... }:
+  outputs = { self, nixpkgs, flake-utils, cargo2nix, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ (import rust-overlay) ];
-          config.allowUnfree = true;
+          overlays = [cargo2nix.overlays.default];
         };
 
-        inherit (pkgs) lib;
-
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-        # Filter sources that requre rebuilds on change
-        src = lib.cleanSourceWith {
-          src = craneLib.path ./.; # The original, unfiltered source
-          filter = path: type:
-            (lib.hasSuffix "\.html" path) ||
-            (lib.hasSuffix "\.sql" path) ||
-            (lib.hasSuffix "\.css" path) ||
-            (lib.hasSuffix "\.ico" path) ||
-            # Default filter from crane (allow .rs files)
-            (craneLib.filterCargoSources path type)
-          ;
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.75.0";
+          packageFun = import ./Cargo.nix;
         };
-
-        # Common arguments can be set here to avoid repeating them later
-        commonArgs = {
-          inherit src;
-          strictDeps = true;
-          nativeBuildInputs = [
-            pkgs.pkg-config
-          ];
-          buildInputs = [
-            pkgs.openssl
-          ];
-        };
-
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
-
-        # Build *just* the cargo dependencies, so we can reuse
-        # all of that work (e.g. via cachix) when running in CI
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-
-        # Build the actual crate itself, reusing the dependency
-        # artifacts from above.
-        feaston = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-
-          nativeBuildInputs = (commonArgs.nativeBuildInputs or []) ++ [
-            pkgs.sqlx-cli
-          ];
-
-          preBuild = ''
-            cp -r templates $out/
-            cp -r assets $out/
-            cp -r migrations $out/
-            export DATABASE_URL=sqlite:./db.sqlite3
-            sqlx database create
-            sqlx migrate run
-          '';
-
-          # postInstall = ''
-          #   cp -r templates $out/
-          #   cp -r assets $out/
-          #   cp -r migrations $out/
-          # '';
-        });
-      in
+      in rec
       {
-        checks = {
-          inherit feaston;
-        };
-
         packages = {
-          default = feaston;
-          inherit feaston;
+          # Build the actual crate itself, reusing the dependency
+          # artifacts from above.
+          feaston = (rustPkgs.workspace.feaston {
+            buildInputs = [
+              pkgs.sqlx-cli
+            ];
+            buildPhase = ''
+              cp -r templates $out/
+              cp -r assets $out/
+              cp -r migrations $out/
+              export DATABASE_URL=sqlite:./db.sqlite3
+              sqlx database create
+              sqlx migrate run
+              runHook preBuild
+              runHook overrideCargoManifest
+              runHook setBuildEnv
+              runHook runCargo
+              runHook postBuild
+            '';
+          });
+          default = packages.feaston;
         };
 
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
+        # devShells.default = craneLib.devShell {
+        #   # Inherit inputs from checks.
+        #   checks = self.checks.${system};
 
-          # Additional dev-shell environment variables can be set directly
-          # PROJECT_ID = "voltaic-layout-417220";
-          DATABASE_URL="sqlite:./sqlite.db";
+        #   # Additional dev-shell environment variables can be set directly
+        #   # PROJECT_ID = "voltaic-layout-417220";
+        #   DATABASE_URL="sqlite:./sqlite.db";
           
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = with pkgs; [
-            sqlx-cli
-            rustywind # CLI for organizing Tailwind CSS classes
-            tailwindcss
-            bacon
-            cargo-watch
-            systemfd
-            just
-            rust-analyzer
-            rustfmt
-            tailwindcss-language-server
-            nodePackages.vscode-langservers-extracted
-          ];
-        };
+        #   # Extra inputs can be added here; cargo and rustc are provided by default.
+        #   packages = with pkgs; [
+        #     sqlx-cli
+        #     rustywind # CLI for organizing Tailwind CSS classes
+        #     tailwindcss
+        #     bacon
+        #     cargo-watch
+        #     systemfd
+        #     just
+        #     rust-analyzer
+        #     rustfmt
+        #     tailwindcss-language-server
+        #     nodePackages.vscode-langservers-extracted
+        #   ];
+        # };
       }
     );
 }
