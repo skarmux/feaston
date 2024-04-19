4f8adafcf76385f8f4ec43a8c49a7b4de9947718
Nils Harbke <nils.harbke@proton.me>
2024-04-19 13:28:55 +0200

    reduce amount of supported systems

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane, ... }:
-    flake-utils.lib.eachDefaultSystem (localSystem:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (localSystem:
       let
         crossSystem = "aarch64-linux";
 
