727a2c7648a9b8c2745e3ac7e8e840f00682eff2
Nils Harbke <nils.harbke@proton.me>
2024-04-19 15:05:11 +0200

    update

--- flake.nix
+++ flake.nix
@@ -130,7 +130,54 @@
         feaston = pkgs.callPackage crateExpression { };
       in
       {
-        nixosModules.default = import ./modules/feaston self;
+        nixosModules.default = { pkgs, config, lib, ... }: {
+          options.services.feaston = {
+            enable = lib.mkEnableOption ''
+              Feaston event contribution planner     
+            '';
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${pkgs.system}.default;
+              description = ''
+                The package to use with the service.
+              '';
+            };
+          }; # options.services.feaston
+          config = lib.mkIf config.service.feaston.enable {
+            users.users.feaston = {
+              description = "Feaston daemon user";
+              isSystemUser = true;
+              password = "";
+              group = "feaston";
+
+              # Whether to enable lingering for this user. If true, systemd user units
+              # will start at boot, rather than starting at login and stopping at logout.
+              # This is the declarative equivalent of running loginctl enable-linger for 
+              # this user.
+              linger = true;
+            };
+
+            users.groups."feaston" = {};
+
+            systemd.services.feaston = {
+              description = "Feaston event contribution planner";
+
+              after = [ "network-online.target" ];
+              wants = [ "network-online.target" ];
+              wantedBy = [ "multi-user.target" ];
+
+              serviceConfig = {
+                User = "feaston";
+                Group = "feaston";
+                Restart = "always";
+                ExecStart = "${config.services.feaston.package}/bin/feaston";
+                StateDirectory = "feaston";
+                StateDirectoryMode = "0750";        
+              };
+            }; # systemd.services.feaston
+          }; # config
+        };
 
         checks = {
           inherit feaston;
@@ -138,7 +185,7 @@
         # checks = builtins.mapAttrs (system: deployLib: deployLib.deployChecks self.deploy) deploy-rs.lib;
 
         packages = {
-          aarch64-linux.default = feaston;
+          "aarch64-linux".default = feaston;
         };
 
         # devShells.default = craneLib.devShell {
