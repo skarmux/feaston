7c9b7976fdb6144e2a84a59dfd60169e01a0dfe3
Nils Harbke <nils.harbke@proton.me>
2024-04-20 16:25:50 +0200

    package as user systemd service

--- flake.nix
+++ flake.nix
@@ -15,61 +15,11 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = inputs@{ self, nixpkgs, crane, fenix, flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+  outputs = inputs@{ self, nixpkgs, crane, fenix, flake-parts, ... }:
+  flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" ];
-    flake = {
-      nixosModules.default = { pkgs, config, lib, ... }:
-      let
-        cfg = config.services.feaston;
-      in {
 
-        options.services.feaston = {
-          enable = lib.mkEnableOption ''
-            Feaston event contribution planner     
-          '';
-          package = lib.mkOption {
-            type = lib.types.package;
-            default = self.packages.${pkgs.system}.default;
-            description = ''
-              The package to use with the service.
-            '';
-          };
-        }; # options.services.feaston
-
-        config = lib.mkIf cfg.enable {
-
-          users.users.feaston = {
-            description = "Feaston daemon user";
-            isSystemUser = true;
-            password = "";
-            group = "feaston";
-
-            # Whether to enable lingering for this user. If true, systemd user units
-            # will start at boot, rather than starting at login and stopping at logout.
-            # This is the declarative equivalent of running loginctl enable-linger for 
-            # this user.
-            linger = true;
-          };
-
-          users.groups."feaston" = {};
-
-          systemd.services.feaston = {
-            description = "Feaston event contribution planner";
-            after = [ "network-online.target" ];
-            wants = [ "network-online.target" ];
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              User = "feaston";
-              Group = "feaston";
-              Restart = "always";
-              ExecStart = "${config.services.feaston.package}/bin/feaston";
-              StateDirectory = "feaston";
-              StateDirectoryMode = "0750";        
-            };
-          }; # systemd.services.feaston
-        }; # config
-      }; # nixosModules.default
-    }; # flake
+    flake = {};
 
     perSystem = { pkgs, config, system, ... }:
     let
@@ -126,9 +76,32 @@
       });
     in {
 
-      packages = {
-        default = feaston;
-      };
+      packages.default =
+        let
+          generateSystemd = type: name: config:
+            (nixpkgs.lib.nixosSystem {
+              inherit system;
+              modules = [{ systemd."${type}s".${name} = config; }];
+            }).config.systemd.units."${name}.${type}".text;
+
+          mkService = generateSystemd "service";
+
+          service = pkgs.writeTextFile {
+            name = "feaston.service";
+            text = mkService "feaston" {
+              unitConfig.WantedBy = [ "multi-user.target" ];
+              path = [ feaston ];
+              script = "feaston";
+            };
+          };
+        in 
+        pkgs.writeShellScriptBin "activate" ''
+          mkdir -p $HOME/.config/systemd/user
+          rm $HOME/.config/systemd/user/feaston.service
+          ln -s ${service} $HOME/.config/systemd/user/feaston.service
+          systemctl --user daemon-reload
+          systemctl --user restart feaston
+        '';
 
       devShells.default = craneLib.devShell {
         
