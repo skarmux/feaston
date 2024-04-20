34d6806c02f01283713dafdb6c5992d7b5e9eefc
Nils Harbke <nils.harbke@proton.me>
2024-04-20 18:04:04 +0200

    return nixosModules user config

--- flake.nix
+++ flake.nix
@@ -19,7 +19,36 @@
   flake-parts.lib.mkFlake { inherit inputs; } {
     systems = [ "x86_64-linux" "aarch64-linux" ];
 
-    flake = {};
+    flake = {
+      nixosModules.default = { pkgs, config, lib, ... }:
+      let
+        cfg = config.services.feaston;
+      in {
+        options.services.feaston = {
+          enable = lib.mkEnableOption ''
+            Feaston event contribution planner     
+          '';
+        }; # options.services.feaston
+
+        config = lib.mkIf cfg.enable {
+
+          users.users.feaston = {
+            description = "Feaston daemon user";
+            isSystemUser = true;
+            password = "";
+            group = "feaston";
+
+            # Whether to enable lingering for this user. If true, systemd user units
+            # will start at boot, rather than starting at login and stopping at logout.
+            # This is the declarative equivalent of running loginctl enable-linger for 
+            # this user.
+            linger = true;
+          };
+
+          users.groups."feaston" = {};
+        };
+      };
+    };
 
     perSystem = { pkgs, config, system, ... }:
     let
