cf2f63ed77916d76f611b41f23c935e053d0f822
Nils Harbke <nils.harbke@proton.me>
2024-05-29 22:25:18 +0200

    return all option settings

--- modules/feaston/default.nix
+++ modules/feaston/default.nix
@@ -1,19 +1,68 @@
 { config, lib, pkgs, ... }:
+let
+  inherit (lib) mkOption types mkEnableOption;
+  cfg = config.feaston;
+in
 {
-    systemd.services.feaston = {
-        description = "Feast On event contribution planner";
-
-        after = [ "network-online.target" ];
-        wants = [ "network-online.target" ];
-        wantedBy = [ "multi-user.target" ];
-
-        serviceConfig = {
-            User = "feaston";
-            Group = "feaston";
-            Restart = "always";
-            ExecStart = "${cfg.package}/bin/feaston";
-            StateDirectory = "feaston";
-            StateDirectoryMode = "0750";        
+    options.feaston = {
+        enable = mkEnableOption ''
+          Feaston event contribution planner     
+        '';
+        package = mkOption {
+            type = types.package;
+            default = flake.packages.${pkgs.system}.default;
+            description = ''
+                The package to use with the service.
+            '';
+        };
+        domain = mkOption {
+            type = types.str;
+            default = "feaston.ddns.net";
+            description = ''
+                Domain from which the service will be exposed.
+            '';
+        };
+        port = mkOption {
+            type = types.int;
+            default = 5000;
+            description = ''
+                Port where the service is exposed locally.
+            '';
+        };
+        databaseURL = mkOption {
+            type = types.str;
+            default = "/var/feaston/db.sqlite";
+            description = ''
+                Location of the sqlite database.
+            '';
         };
     };
+
+    config = lib.mkIf cfg.enable {
+        services.nginx = {
+            enable = true;
+            virtualHosts = {
+                cfg.domain = {
+                    locations."/".proxyPass = "http://127.0.0.1:${toString cfg.port}";
+                };
+            };
+        };
+        systemd.services.feaston = {
+            description = "Feast On event contribution planner";
+
+            after = [ "network-online.target" ];
+            wants = [ "network-online.target" ];
+            wantedBy = [ "multi-user.target" ];
+
+            serviceConfig = {
+                User = "feaston";
+                Group = "feaston";
+                Restart = "always";
+                ExecStart = "${cfg.package}/bin/feaston";
+                StateDirectory = "feaston";
+                StateDirectoryMode = "0750";        
+            };
+        };
+    };
+    
 }
