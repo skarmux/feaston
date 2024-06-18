17fd261f90e487ac6ba17c9f7d43dff0112d03b0
Nils Harbke <nils.harbke@proton.me>
2024-06-19 01:03:49 +0200

    use user unit instead of service

--- modules/feaston/default.nix
+++ modules/feaston/default.nix
@@ -13,8 +13,8 @@ in
         package = lib.mkOption {
           type = lib.types.package;
           default = if cfg.enableNginx 
-            then inputs.self.packages.${system}.feaston-nginx 
-            else inputs.self.packages.${system}.feaston;
+          then inputs.self.packages.${system}.feaston-nginx 
+          else inputs.self.packages.${system}.feaston;
           description = ''
           The package to use with the service.
           '';
@@ -53,57 +53,35 @@ in
 
     config = lib.mkIf cfg.enable {
 
-      systemd.services.feaston = {
-        wantedBy = [ "multi-user.target" ];
-        environment.RUST_LOG = cfg.logLevel;
-        serviceConfig = {
-          User = defaultUser;
-          Group = defaultUser;
-          ExecStart = "${cfg.package}/bin/feaston --database-url ${cfg.database.url} --port ${toString cfg.port}";
+      systemd.user.units.${defaultUser} = {
+        name = "feaston";
+        wantedBy = [ "default.target" ];
+        # environment.RUST_LOG = cfg.logLevel;
+        text = ''
+        [Unit]
+        Description=Serve Feast-On web service.
+        StartLimitIntervalSeconds=30
+        StartLimitBurst=2
 
-            # hardening
-            RemoveIPC = true;
-            CapabilityBoundingSet = [ "" ];
-            DynamicUser = true;
-            NoNewPrivileges = true;
-            PrivateDevices = true;
-            ProtectClock = true;
-            ProtectKernelLogs = true;
-            ProtectControlGroups = true;
-            ProtectKernelModules = true;
-            SystemCallArchitectures = "native";
-            MemoryDenyWriteExecute = true;
-            RestrictNamespaces = true;
-            RestrictSUIDSGID = true;
-            ProtectHostname = true;
-            LockPersonality = true;
-            ProtectKernelTunables = true;
-            RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" ];
-            RestrictRealtime = true;
-            ProtectSystem = "strict";
-            ProtectProc = "invisible";
-            ProcSubset = "pid";
-            ProtectHome = true;
-            PrivateUsers = true;
-            PrivateTmp = true;
-            SystemCallFilter = [ "@system-service" "~ @privileged @resources" ];
-            UMask = "0077";
+        [Service]
+        ExecStart = ${cfg.package}/bin/feaston --database-url ${cfg.database.url} --port ${toString cfg.port}
+        Restart = "on-failure"
+        '';
+      };
+
+      services.nginx.virtualHosts = lib.mkIf cfg.enableNginx {
+        "${cfg.domain}" = {
+          enableACME = cfg.enableTLS;
+          forceSSL = cfg.enableTLS;
+          root = "${inputs.self.packages.${system}.feaston-nginx}/www";
+          locations."/" = {
+            tryFiles = "$uri $uri/ /index.html";
           };
-        };
-
-        services.nginx.virtualHosts = lib.mkIf cfg.enableNginx {
-          "${cfg.domain}" = {
-            enableACME = cfg.enableTLS;
-            forceSSL = cfg.enableTLS;
-            root = "${inputs.self.packages.${system}.feaston-nginx}/www";
-            locations."/" = {
-              tryFiles = "$uri $uri/ /index.html";
-            };
-            locations."/api/" = {
-              proxyPass = "http://127.0.0.1:${toString cfg.port}/";
-              recommendedProxySettings = true;
-            };
+          locations."/api/" = {
+            proxyPass = "http://127.0.0.1:${toString cfg.port}/";
+            recommendedProxySettings = true;
           };
         };
       };
-    }
+    };
+  }
